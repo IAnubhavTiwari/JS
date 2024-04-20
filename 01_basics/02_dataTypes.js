00f980d26e04edf86196a8fc3058ccf9fc58f000
Anubhav Tiwari <159955143+IAnubhavTiwari@users.noreply.github.com>
2024-04-20 01:48:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as new version
+
+// alert(3+3) // We can't do that because we are not using browser, er are using node.js
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+     let name = "Anubhav"
+     let age = 19
+     let isLoggedIn = false
+     let state; // undefined
+     
+     // number => 2 to power 53
+     // bigint => very large number
+     // string => ""
+     // boolean => true/false
+     // null => standalone value
+     // undefined => we have not defined the value
+     // symbol => unique
+     
+     
+     // object
+     
+     console.log(typeof undefined); // undefined
+     console.log(typeof null); // object   
