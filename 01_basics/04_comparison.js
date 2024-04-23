bcb2eb12b8e642baacf2a534d8d4a697f66f9a1e
Anubhav Tiwari <159955143+IAnubhavTiwari@users.noreply.github.com>
2024-04-23 19:44:01 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+/*
+console.log(2 > 1); true
+console.log(2 >= 1); true
+console.log(2 < 1); false
+console.log(2 == 1); false
+console.log(2 != 1 ); true
+*/
+
+console.log("02" > 1); // true
+console.log(null > 0); //false
+console.log(null >= 0); //true
+console.log(null == 0); //false (null is converted into 0)
+
+// === 
+
+console.log("2" === 2);
